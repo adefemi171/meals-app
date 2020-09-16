478ecb093b362e996207eaa535026b0d1bd4dc25
adefemi171 <adefemi171@gmail.com>
2020-09-16 19:12:51 -0400

    Added route navigation from categoryScreen to CategoryMealsScreen

--- src/screens/CategoriesPage.js
+++ src/screens/CategoriesPage.js
@@ -1,13 +1,17 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Button } from 'react-native'
 
 
 
 
 const CategoriesPage = props => {
+    // console.log(props)
     return (
         <View style={styles.container}>
             <Text> Categories Page! </Text>
+            <Button title="Go to Meals Page" onPress={() => {
+                props.navigation.navigate({routeName: 'CategoryMeals'})
+            }} />
         </View>
     );
 };
