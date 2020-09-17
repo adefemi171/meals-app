dfd5cf3fb96929501e67a331a297a8bd7125f1ae
adefemi171 <adefemi171@gmail.com>
2020-09-16 20:52:40 -0400

    used navigationOptions as a function in CategoryMealsPage add headerTitle, headerStyle and headerTintColor to CategoryMealsPage

--- src/screens/CategoryMealsPage.js
+++ src/screens/CategoryMealsPage.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { View, Text, Button, StyleSheet } from 'react-native'
+import { View, Text, Button, StyleSheet, Platform } from 'react-native'
 
 import { CATEGORIES } from '../data/dummy-data'
+import Colors from '../constants/Color'
 
 
 const CategoryMealsPage = props => {
@@ -22,7 +23,31 @@ const CategoryMealsPage = props => {
     );
 };
 
+// accessing the function CategoryMealsPage as an object to set the title
+// navigationOptions can either be an object if there's static hardcoded config
+// or can be a function if one need a dynamic config that depends on changing data
+// since the data is changing due to different id, title from CATEGORIES
+CategoryMealsPage.navigationOptions = (navigationData) => {
+    // console.log(navigationData)
+    
+    // navigationData can then be used to acces the getParam() function
+    //getParam() a method provided to extract parameters received in this case from CategoriesPage
+    const navigationDataFoodCatId = navigationData.navigation.getParam('foodCategoryId')
 
+    // function to find selected food category and returns item where the function is true
+    //find takes a function and executes on every element in the array
+    const navigationDataSelectedCategory = CATEGORIES.find(cat => cat.id === navigationDataFoodCatId)
+
+    return{
+        headerTitle: navigationDataSelectedCategory.title,
+        headerStyle:{
+            // Using Platform to switch colors on IOS and ANdroid
+            backgroundColor: Platform.OS === 'android' ? Colors.secondaryColor : ''
+        },
+        headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
+    }
+
+}
 
 const styles = StyleSheet.create({
     container: {
