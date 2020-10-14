763074d5cbd333cb266187f822eb91b57f6fd0e7
adefemi171 <adefemi171@gmail.com>
2020-10-14 12:33:21 -0400

    Installed Redux and React Redux Created a store folder to manage redux set up

---
+++ src/store/reducer/meals.js
@@ -0,0 +1,9 @@
+// file to manage meal reducer updating logic
+// manage filters and favorites
+
+// function that receives two arguments
+// current state snapshot and the action
+
+const mealsReducer = (state, action) => {
+    return state
+}
